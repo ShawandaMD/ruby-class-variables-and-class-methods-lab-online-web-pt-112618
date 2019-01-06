0e78ee92e743eb390fb044a4ce1506f452e16f7c
Shawanda Davis <shawandadavis93@gmail.com>
2019-01-06 19:14:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,37 @@
+class Song 
+  attr_reader :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end   
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+  def self.count
+    @@count
+  end  
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    genre_count = @@genres.group_by{ |genre| genre}.map{|genre, num| [genre, num.size]}.to_h
+  end  
+  
+  def self.artist_count
+  
+  end
+
+end  
